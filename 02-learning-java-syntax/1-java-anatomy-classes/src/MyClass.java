7c45480f2c163307a9e67f957639beca4fab3fcf
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2023-02-15 16:37:11 -0300

    Create Java Anatomy Classes

---
+++ 02-learning-java-syntax/1-java-anatomy-classes/src/MyClass.java
@@ -0,0 +1,17 @@
+
+public class MyClass {
+  public static void main(String[] args) {
+
+    String firstName = "Henrique";
+    String secondName = "Schilling";
+
+    String completeName = completeName(firstName, secondName);
+    System.out.println(completeName);
+
+  }
+
+  public static String completeName(String firstName, String secondName) {
+    return "Method result is: " + firstName.concat(" ").concat(secondName);
+
+  }
+}
