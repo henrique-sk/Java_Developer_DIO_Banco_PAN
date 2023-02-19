c876c22fe56055ecf50f85fc307f4fda319e3bdb
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2023-02-19 14:38:30 -0300

    Create Ex3_RandomNumbers.java

---
+++ 01-Getting-to-Know-the-Java/c4-repetition-structures-and-arrays/src/a1_arrays/Ex3_RandomNumbers.java
@@ -0,0 +1,33 @@
+package a1_arrays;
+
+import java.util.Random;
+
+/*
+Make a program that reads 20 random integers (between 0 and 100) and stores them in an array.
+At the end show the numbers and their successors.
+*/
+
+public class Ex3_RandomNumbers {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    int[] randomNumbers = new int[20];
+    
+    for (int i = 0; i < randomNumbers.length ; i++) {
+      randomNumbers[i] = random.nextInt(101);
+    }
+    
+    System.out.print("The numbers are: ");
+    for (int randomNumber : randomNumbers) {
+      System.out.print(randomNumber + " ");
+    }
+    
+    System.out.print("\nThe successors are: ");
+    for (int randomNumber : randomNumbers) {
+      System.out.print((randomNumber + 1) + " ");
+    }
+    
+
+  }
+
+}
