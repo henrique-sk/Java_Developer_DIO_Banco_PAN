9c391c78dc30fa9f8ac47fca1fa08d8f50ffdbcb
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2023-02-19 14:54:37 -0300

    Update Ex3_RandomNumbers.java

--- 01-Getting-to-Know-the-Java/c4-repetition-structures-and-arrays/src/a1_arrays/Ex3_RandomNumbers.java
+++ 01-Getting-to-Know-the-Java/c4-repetition-structures-and-arrays/src/a1_arrays/Ex3_RandomNumbers.java
@@ -12,22 +12,20 @@ public class Ex3_RandomNumbers {
   public static void main(String[] args) {
     Random random = new Random();
     int[] randomNumbers = new int[20];
-    
-    for (int i = 0; i < randomNumbers.length ; i++) {
+
+    for (int i = 0; i < randomNumbers.length; i++) {
       randomNumbers[i] = random.nextInt(101);
     }
-    
+
     System.out.print("The numbers are: ");
     for (int randomNumber : randomNumbers) {
       System.out.print(randomNumber + " ");
     }
-    
+
     System.out.print("\nThe successors are: ");
     for (int randomNumber : randomNumbers) {
       System.out.print((randomNumber + 1) + " ");
     }
-    
-
   }
 
 }
