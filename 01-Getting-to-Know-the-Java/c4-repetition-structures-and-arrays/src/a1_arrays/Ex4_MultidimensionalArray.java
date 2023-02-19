bc222ecd1905d7cd00c17c46d0e9ae80212bf6e5
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2023-02-19 14:54:42 -0300

    Create Ex4_MultidimensionalArray.java

---
+++ 01-Getting-to-Know-the-Java/c4-repetition-structures-and-arrays/src/a1_arrays/Ex4_MultidimensionalArray.java
@@ -0,0 +1,30 @@
+package a1_arrays;
+
+import java.util.Random;
+
+/*
+Generate and print a 4x4 matrix M with random values between 0-9.
+*/
+
+public class Ex4_MultidimensionalArray {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    
+    int[][] M = new int[4][4];
+    
+    for (int i = 0; i < M.length; i++) {
+      for (int j = 0; j < M[i].length; j++) {
+        M[i][j] = random.nextInt(10);
+      }
+    }
+    
+    for (int[] row : M) {
+      for(int column : row) {
+        System.out.print(column + " ");
+      }
+      System.out.println();
+    }
+  }
+
+}
