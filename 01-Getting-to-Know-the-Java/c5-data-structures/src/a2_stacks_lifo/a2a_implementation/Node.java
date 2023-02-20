ae07d174878139402e3664a19b3373682e1fae16
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2023-02-20 17:28:10 -0300

    Implementing stacks

---
+++ 01-Getting-to-Know-the-Java/c5-data-structures/src/a2_stacks_lifo/a2a_implementation/Node.java
@@ -0,0 +1,5 @@
+package a2_stacks_lifo.a2a_implementation;
+
+public class Node {
+
+}
---
+++ 01-Getting-to-Know-the-Java/c5-data-structures/src/a2_stacks_lifo/a2a_implementation/Stack.java
@@ -0,0 +1,10 @@
+package a2_stacks_lifo.a2a_implementation;
+
+public class Stack {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
